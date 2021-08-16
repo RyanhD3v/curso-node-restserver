e2032c32c39d5b73c331b67c03d66cb98c447edd
RyanhD3v <nestorhugobarralez@gmail.com>
2021-08-16 16:29:50 -0500

    Uploaded cloudinary added

--- routes/uploads.js
+++ routes/uploads.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { validarCampos, validarArchivoSubir } = require('../middlewares');
-const { cargarArchivo, actualizarImagen, mostrarImagen } = require('../controllers/uploads');
+const { cargarArchivo, actualizarImagen, mostrarImagen, actualizarImagenCloudinary } = require('../controllers/uploads');
 const { coleccionesPermitidas } = require('../helpers');
 
 
@@ -15,7 +15,8 @@ router.put('/:coleccion/:id', [
     check('id','El id debe ser de Mongo').isMongoId(),
     check('coleccion').custom( c => coleccionesPermitidas(c,['usuarios','productos'] ) ),
     validarCampos
-], actualizarImagen )
+], actualizarImagenCloudinary )
+//], actualizarImagen )
 
 router.get('/:coleccion/:id', [
     check('id','El id debe ser de Mongo').isMongoId(),
